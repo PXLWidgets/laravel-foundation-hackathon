ee7d7e47706ff636edca2c3176227846b53ded00
Arto Rozenga <arto@widgets.nl>
2019-06-21 18:04:48 +0200

    show correct badge

--- resources/views/partials/badges-list.blade.php
+++ resources/views/partials/badges-list.blade.php
@@ -1,7 +1,13 @@
 <ul class="badges-list">
     @foreach ($badges as $badge)
+        @php
+            /** @var \App\User $user */
+            $isEarned = $user->badges->contains(function (\App\Badge $b) use ($badge) {
+                return $b->id === $badge->id;
+            })
+        @endphp
         <li>
-            <img alt="{{$badge->title}}" src="{{ str_replace('localhost', 'localhost:8011', $badge->image->url()) }}" class="grey img-fluid"/>
+            <img alt="{{$badge->title}}" src="{{ str_replace('localhost', 'localhost:8011', $badge->image->url()) }}" class="@unless($isEarned) grey @endunless img-fluid"/>
         </li>
     @endforeach
 </ul>
