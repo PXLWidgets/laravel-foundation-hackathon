fb7afca54201b748c6654745ed3d8fe3c280a43c
Jeffrey Westerkamp <jeffrey@pxlwidgets.com>
2019-06-21 17:12:11 +0200

    Some static homepage content

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,6 +7,7 @@
         <div class="intro">
 
             <h1>Laravel SkillTree</h1>
+
             <p>
                 Een slimme, snelle meetbare manier om meer developers,
                 ervaren of beginnend, kennis te laten maken met het
@@ -14,13 +15,32 @@
             </p>
         </div>
 
-        <div class="row">
-            <div class="col-lg-6">
+        <br><br>
 
+        <div class="row flex-row align-items-center">
+            <div class="col-sm-6 col-lg-4">
+                <img src="/images/badges/b1.jpg" alt="">
             </div>
 
-            <div class="col-lg-6">
-                <p></p>
+            <div class="col-sm-6 col-lg-4">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+            </div>
+        </div>
+
+        <div class="row flex-row align-items-center">
+
+            <div class="col-sm-6 col-lg-4">
+                <p>
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat.
+                </p>
+            </div>
+
+            <div class="col-sm-6 col-lg-4">
+                <img src="/images/badges/b2.jpg" alt="">
             </div>
         </div>
 
